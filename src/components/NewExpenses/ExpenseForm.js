3c9ee7afc334017e363c54dd1baac8b4a0fb1ef2
ChappaAbhiram <abhiramchappa@gmail.com>
2023-07-23 17:14:38 +0530

    Added states for date,title and amount

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import './ExpenseForm.css';
 const ExpenseForm = ()=>{
+    const [enteredtitle,setnewTitle] = useState('');
+    const [enteredDate,setnewDate] = useState();
+    const [changeamount,setnewAmount] = useState();
+    
     const titlechangeHandler = (event) =>{
-     console.log(event.target.value);
+     setnewTitle(event.target.value);
+    };
+    const datechangeHandler = (event) =>{
+        setnewDate(event.target.value);
+    }
+    const amountchangeHandler = (event) =>{
+        setnewAmount(event.target.value);
     }
 return <form>
   <div className="new-expense__controls">
@@ -12,11 +23,11 @@ return <form>
     </div>
     <div className="new-expense__control">
         <label>Amount</label>
-        <input type='number' min='0.01' step='0.01' />
+        <input type='number' onChange={amountchangeHandler}min='0.01' step='0.01' />
     </div>
     <div className="new-expense__control">
         <label>Date</label>
-        <input type='date' min='2023-01-01' max='2027-12-31'/>
+        <input type='date' onChange={datechangeHandler} min='2023-01-01' max='2027-12-31'/>
     </div>
   </div>
   <div className="new-expense__actions">
