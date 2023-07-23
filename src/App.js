742d757dca653017028a94db26ac1160f179b4ee
ChappaAbhiram <abhiramchappa@gmail.com>
2023-07-23 16:58:24 +0530

    Form is enabled on screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import React from 'react'
+import NewExpense from './components/NewExpenses/NewExpense'
 function App() {
   const expenses = [
     {
@@ -50,8 +51,8 @@ function App() {
 //  );
   return (
     <div>
-      <h2>Let's get started!</h2>
-      {expenseitems};
+      <NewExpense />
+      {expenseitems}
     </div>
   );
 }
---
+++ src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm'
+import './NewExpense.css';
+const NewExpense=()=>{
+    return <div class="new-expense">
+        <ExpenseForm />
+    </div>
+
+};
+export default NewExpense;
